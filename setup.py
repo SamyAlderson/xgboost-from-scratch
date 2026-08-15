ae5ba722daae6f69458ffe2378eb2c9ef37c4136
Samy Alderson <samysam35000@gmail.com>
2026-08-15 06:05:45 +0200

    Improved setup.py for Python 3.9 compatibility

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
 from setuptools import setup, find_packages
 
-# Specify project metadata
 NAME = 'xgboost-from-scratch'
 VERSION = '1.0'
 
-# Specify dependencies
 DEPENDENCIES = [
     'numpy',
     'scipy',
     'pandas'
 ]
 
-# Specify package structure
 PACKAGES = find_packages('src')
 
-# Specify package structure relative to this setup file
 PACKAGE_DIR = {'': 'src'}
 
-# Specify entry points
 ENTRY_POINTS = {
     'console_scripts': [
         'xgboost = src.main:main',
@@ -31,4 +26,6 @@ setup(
     package_dir=PACKAGE_DIR,
     install_requires=DEPENDENCIES,
     entry_points=ENTRY_POINTS,
+    include_package_data=True,
+    zip_safe=False
 )
