0de48b848439e35e1461ce9bc31787b30a67a346
Samy Alderson <samysam35000@gmail.com>
2026-08-15 23:24:10 +0200

    Updated setup.py to use wheel distribution for pypi upload

--- setup.py
+++ setup.py
@@ -27,5 +27,16 @@ setup(
     install_requires=DEPENDENCIES,
     entry_points=ENTRY_POINTS,
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
